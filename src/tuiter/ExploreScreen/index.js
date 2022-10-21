fb8cd7f6a63a7f6f85cc98c8662ed98ecc65b025
Alice <alice@Alices-MacBook-Pro.local>
2022-10-20 20:39:49 -0700

    bug fixed

--- src/tuiter/ExploreScreen/index.js
+++ src/tuiter/ExploreScreen/index.js
@@ -10,6 +10,7 @@ const ExploreScreen = () => {
         <div className="row mt-2">
             <div className="col-2 col-md-2 col-lg-1 col-xl-2">
                 <NavigationSidebar active="explore"/>
+                <Link to="/">Back to Lab</Link>
             </div>
             <div className="col-10 col-md-10 col-lg-7 col-xl-6"
                  style={{"position": "relative"}}>
@@ -18,7 +19,6 @@ const ExploreScreen = () => {
             <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
                 <WhoToFollowList/>
             </div>
-            <Link to="/">Back</Link>
         </div>
 
     );
--- src/tuiter/HomeScreen/index.js
+++ src/tuiter/HomeScreen/index.js
@@ -11,7 +11,7 @@ const HomeScreen = () => {
         <div className="row mt-1">
             <div className="pt-3 col-xxl-2 col-xl-2 col-lg-1 col-md-2 col-sm-2 col-2 ">
                 <NavigationSidebar active="home" />
-
+                <Link to="/">Back to Lab</Link>
             </div>
             <div className="pt-3 col-xxl-6 col-xl-6 col-lg-7 col-md-10 col-sm-10 col-10 wd-flex-column wd-borderDiv border-top-0 border-bottom-0">
                 <HomeComponent />
@@ -19,7 +19,7 @@ const HomeScreen = () => {
             <div className="d-none d-md-none d-lg-block col-lg-4 col-xl-4 pt-3 col-xxl-3 ">
                 <PostSummaryList src='home' />
             </div>
-            <Link to="/">Back</Link>
+
         </div>
 
     );
